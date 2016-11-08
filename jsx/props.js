3347308f8d93c4ac399a1daca11efcb317236514
dearzoe <2421113739@qq.com>
2016-11-08 22:42:21 +0800

    props

---
+++ jsx/props.js
@@ -0,0 +1,22 @@
+var Message= React.createClass({
+    getInitialState: function () {
+        return {
+            message: ['你好', '世界', '!!!']
+        }
+    },
+    render: function () {
+        return <div>
+                   <h1>HelloWorld!</h1>
+                   <Little messages={this.state.message}/>
+               </div>
+    }
+});
+var Little=React.createClass({
+    render: function () {
+       return <h1>{this.props.messages}</h1>
+    }
+});
+ReactDOM.render(
+  <Message/>,
+  document.getElementById("app")
+);
--- jsx/states.js
+++ jsx/states.js
@@ -7,9 +7,10 @@ var Message= React.createClass({
     },
     render: function () {
         var styleObj={
-            color:this.state.isVisible?'red':yellow,
+            color:this.state.isVisible?'red':'yellow'
         }
         return <div>
+                    <button onClick={this.handleClick}>点击我</button>
                     <h1 style={styleObj}>{this.state.titleMessage} </h1>
                     <Little/>
                </div>
@@ -21,8 +22,16 @@ var Little=React.createClass({
                    有啥可以hello的
                </h2>
     }
-})
-ReactDOM.render(
+});
+var box=ReactDOM.render(
   <Message/>,
-  document.getElementById("app")
+  document.getElementById("app"),
+    function () {
+        console.log("渲染完成")
+    }
 );
+//设置state  (一步)
+box.setState({titleMessage:"hahaha"});
+//设置state  (两步)
+box.state.isVisible = false;
+box.forceUpdate();
