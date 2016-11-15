14b96fbb063b7958b6af499fbcea6f70f03c466d
dearzoe <2421113739@qq.com>
2016-11-15 22:55:42 +0800

    onchange

--- jsx/events.js
+++ jsx/events.js
@@ -1,15 +1,26 @@
 var FormApp = React.createClass({
-    getinitialState: function () {
+    getInitialState: function () {
         return {
             inp:"input value",
             sel:"B",
             rad:"B"
         }
     },
+    handleChange: function (e) {
+      this.setState({
+          inp:e.target.value,
+
+      })
+    },
+    handleSubmit: function (e) {
+        e.preventDefault();/*这里必须加 e.preventDefault(); 阻止默认事件*/
+        console.log(e);
+        console.log("ok");
+    },/*首先这里执行，不能加 括号（）*/
     render: function () {
-        return <form onsubmit="">
-                    <input type="text"/>
-                    <select name="" id="" defaultValue="B">
+        return <form onSubmit={this.handleSubmit}>
+                    <input type="text" value={this.state.inp} onChange={this.handleChange}/><br/>
+                    <select name="" id="" value={this.state.sel}>
                         <option value="A">A</option>
                         <option value="B">B</option>
                         <option value="C">C</option>
@@ -18,11 +29,11 @@ var FormApp = React.createClass({
                         <option value="F">F</option>
                         <option value="G">G</option>
                         <option value="H">H</option>
-                    </select>
+                    </select><br/>
                     <input name="haha" type="radio" value="A"/>
-                    <input name="haha" type="radio" defaultChecked value="B"/>
-                    <input name="haha" type="radio" value="C"/>
-
+                    <input name="haha" type="radio" defaultChecked value="B" />
+                    <input name="haha" type="radio" value="C"/><br/>
+                    <button type="submit">OK</button>
                </form>
     }
 });
