1d39b2358a3b361633f91793223626d3ea37531d
dearzoe <2421113739@qq.com>
2016-11-16 10:53:07 +0800

    完结

--- jsx/events.js
+++ jsx/events.js
@@ -6,25 +6,25 @@ var FormApp = React.createClass({
             rad:"B"
         }
     },
-    handleInput: function (e) {
+    handleRadio: function (e) {
       this.setState({
-          inp:e.target.value
-      })
-    },
-    handleSelect: function (e) {
-      this.setState({
-         sel:e.target.value
-      })
+           rad:e.target.value
+       })
     },
     handleSubmit: function (e) {
         e.preventDefault();/*这里必须加 e.preventDefault(); 阻止默认事件*/
-        console.log(e);
+        var formData={
+          inp:this.refs["goodInput"].value,
+          sel:this.refs["goodSelect"].value,
+          rad:this.state.rad
+        };
         console.log("ok");
+        console.log(formData)
     },/*首先这里执行，不能加 括号（）*/
     render: function () {
         return <form onSubmit={this.handleSubmit}>
-                    <input type="text" value={this.state.inp} onChange={this.handleInput}/><br/>
-                    <select name="" id="" value={this.state.sel} onChange={this.handleSelect}>
+                    <input ref="goodInput" type="text" defaultValue={this.state.inp}/><br/>
+                    <select ref="goodSelect" name="" id="" defaultValue={this.state.sel}>
                         <option value="A">A</option>
                         <option value="B">B</option>
                         <option value="C">C</option>
@@ -34,9 +34,9 @@ var FormApp = React.createClass({
                         <option value="G">G</option>
                         <option value="H">H</option>
                     </select><br/>
-                    <input name="haha" type="radio" value="A"/>
-                    <input name="haha" type="radio" defaultChecked value="B" />
-                    <input name="haha" type="radio" value="C"/><br/>
+                    <input onChange={this.handleRadio} name="haha" type="radio" value="A"/>
+                    <input onChange={this.handleRadio} name="haha" type="radio" defaultChecked value="B"/>
+                    <input onChange={this.handleRadio} name="haha" type="radio" value="C"/><br/>
                     <button type="submit">OK</button>
                </form>
     }
