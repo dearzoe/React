90235579be54b19ecc5cf1ea3900eacd7bd208d9
dearzoe <2421113739@qq.com>
2016-11-18 16:59:44 +0800

    实战gulp文件

---
+++ reactQa/app/js/components/question.js
@@ -0,0 +1,12 @@
+var React=require("react");
+module.exports=React.createClass({
+    render: function () {
+        return <div>
+                  <div className="jumbotron text-center">
+                      <div className="container">
+                          <h1>React 问答</h1>
+                      </div>
+                  </div>
+               </div>
+    }
+});
