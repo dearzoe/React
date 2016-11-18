c2a3c3c1cec6cdec0962f0da861580d65ba263df
dearzoe <2421113739@qq.com>
2016-11-18 11:02:35 +0800

    valueLink/checkedLink

--- jsx/two-way.js
+++ jsx/two-way.js
@@ -1,3 +1,24 @@
 /**
  * Created by Administrator on 2016/11/18.
  */
+var Twoway = React.createClass({
+   mixins:[React.addons.LinkedStateMixin ],
+   getInitialState:function(){
+     return{
+         message:"这是双向绑定",
+         istrue:"true"
+     }
+   },
+    render: function () {
+        return <span>
+                 <h1>{this.state.message}</h1>
+                 <h1>这是对的么？{this.state.istrue?"是的":"不是"}</h1>
+                 <input type="text" valueLink={this.linkState('message')}/>
+                 <input type="checkbox" checkedLink={this.linkState('istrue')}/>
+               </span>
+    }
+});
+ReactDOM.render(
+    <Twoway/>,
+    document.getElementById("app")
+)
