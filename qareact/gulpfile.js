6f030803d11d4689161e53416c3a2e69777dd833
dearzoe <2421113739@qq.com>
2016-11-19 15:14:40 +0800

    react问答重新创建一个问答

--- qareact/gulpfile.js
+++ qareact/gulpfile.js
@@ -12,43 +12,44 @@ gulp插件
  实时预览 gulp-connect
 
 * */
-/*var gulp = require("gulp"),
-    connect = require("gulp-connect");
-gulp.task("server", function () {
-   connect.server({
-       root:"dist",
-       port:8888,
-       livereload:true
-   })
-});
-gulp.task("html", function () {
-   gulp.src("app/!*.html")
-    .pipe(gulp.dest("dist"))
-    .pipe(connect.reload())
-});
-gulp.task("watch", function () {
-   gulp.watch("app/!*.html",["html"])
-});
-gulp.task("default",["server","watch"]);*/
 
+
+/*
 
 var gulp = require("gulp");
-var connect = require("gulp-connect");
+var connect = require("gulp-connect");//运行liveload服务器
+var browserify=require("gulp-browserify");//拼接在一起让浏览器也可以用require方式构建
+var concat=require("gulp-concat");//讲文件拼接在一起
+
 gulp.task("server", function () {
     connect.server({
         root:"dist",
-        port:7777,
+        port:9090,
         livereload:true
     })
 });
-gulp.task("copy-html", function () {
-   gulp.src('app/*.html').pipe(gulp.dest('dist')).pipe(connect.reload())
+gulp.task("js", function () {
+   gulp.src("./app/!**!/!*.js")
+    .pipe(gulp.dest("./dist"))
+    .pipe(connect.reload())
+});
+gulp.task("html",function(){
+   gulp.src("./app/!**!/!*.html")
+    .pipe(connect.reload())
 });
 gulp.task("watch", function () {
-    gulp.watch('app/*.html',["copy-html"])
+   gulp.watch('./dist/!**!/!*.js',["js"]);
+   gulp.watch('./app/!**!/!*.html',["html"]);
 });
-gulp.task("default",["server","watch","copy-html"]);
-/*gulp.task("server", function () {
+gulp.task("default",["server","watch"]);
+*/
+
+
+var gulp = require("gulp");
+var connect = require("gulp-connect");//运行liveload服务器
+var browserify=require("gulp-browserify");//拼接在一起让浏览器也可以用require方式构建
+var concat=require("gulp-concat");//讲文件拼接在一起
+gulp.task("server", function () {
     connect.server({
         root:'dist',//服务器的根目录
         port:8080,//服务器的地址，没有此配置是8080
@@ -56,18 +57,12 @@ gulp.task("default",["server","watch","copy-html"]);
     })
 });
 gulp.task("copy-html", function () {
-    gulp.src('app/!*.html')
+    gulp.src('app/*.html')
         .pipe(gulp.dest('dist'))
         .pipe(connect.reload())
 });
 gulp.task("watch",function(){
     gulp.watch('app/index.html',["copy-html"])
 });
-gulp.task("default",["server","watch"]);*/
+gulp.task("default",["server","watch"]);
 
-
-/*var gulp= require("gulp");
-gulp.task("cope-html", function () {
-    gulp.src('app/!*.html')
-    .pipe(gulp.dest('dist'))
-})*/
