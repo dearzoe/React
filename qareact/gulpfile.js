256f257f96976dbf5b35fdfda5935fa9245f7a71
dearzoe <2421113739@qq.com>
2016-11-19 11:18:02 +0800

    gulp 使用太好用了

--- qareact/gulpfile.js
+++ qareact/gulpfile.js
@@ -1,4 +1,32 @@
+/*
+gulp插件
+
+ 编译 Sass：gulp-sass
+ 编译 Less：gulp-less
+ 合并文件：gulp-concat
+ 压缩js 文件：gulp-uglify
+ 重命名js文件：gulp-rename
+ 优化图像大小：gulp-imagemin
+ 压缩css 文件：gulp-minify-css
+ 创建本地服务器：gulp-connect
+ 实时预览 gulp-connect
+
+* */
 var gulp = require("gulp");
-gulp.task("hello", function () {
-   console.log("你好") 
+var connect = require("gulp-connect");
+gulp.task("server", function () {
+    connect.server({
+        root:'dist',//服务器的根目录
+        port:8080,//服务器的地址，没有此配置是8080
+        livereload:true
+    })
 });
+gulp.task("html", function () {
+    gulp.src('app/*.html')
+        .pipe(gulp.dest('dist'))
+        .pipe(connect.reload())
+});
+gulp.task("watch",function(){
+    gulp.watch('app/*html',["html"])
+});
+gulp.task("default",["server","watch"]);
