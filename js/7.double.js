99716864de29b7473b1b1c1a0d4b4e8395c171d9
dearzoe <2421113739@qq.com>
2016-11-28 22:13:43 +0800

    input

--- js/7.double.js
+++ js/7.double.js
@@ -5,7 +5,7 @@ let Input = React.createClass({
     },
     //处理input元素值改变事件
     handleChange:function(event){
-        var value = event.target.value;
+        let value = event.target.value;
         this.setState({value:value});
     },
     //如果提供了value 属性，则必须提供onChange属性，否则 此字段变成只读字段不能再编辑
